f5094e38e871fa3878a5c446f5617d5e896b9d3c
yichunzhao <zhaoyichun@hotmail.com>
2020-07-17 09:10:27 +0200

    Initial commit

---
+++ src/main/java/com/ynz/democloud/guestroomservice/GuestRoomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.democloud.guestroomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestRoomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestRoomServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynz/democloud/guestroomservice/GuestRoomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.democloud.guestroomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestRoomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
