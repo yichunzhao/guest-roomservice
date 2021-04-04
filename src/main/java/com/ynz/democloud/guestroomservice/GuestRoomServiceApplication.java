d4d3e0a1713f3eae2e2e4fe270aac533ad862286
yichunzhao <zhaoyichun@hotmail.com>
2021-04-04 19:58:44 +0200

    add spring doc ui, console to h2 etc

--- src/main/java/com/ynz/democloud/guestroomservice/GuestRoomServiceApplication.java
+++ src/main/java/com/ynz/democloud/guestroomservice/GuestRoomServiceApplication.java
@@ -1,15 +1,18 @@
 package com.ynz.democloud.guestroomservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class GuestRoomServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GuestRoomServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+    	log.info("Room domain micro-service:  ");
+        SpringApplication.run(GuestRoomServiceApplication.class, args);
+    }
 
 }
